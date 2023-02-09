709516e7b9c1dcd3f5bdd6f46fb46f35e897f3a9
svalench <chitsalex@gmail.com>
2023-02-09 19:19:16 +0300

    auth login

--- api/auth.py
+++ api/auth.py
@@ -23,6 +23,20 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     return {"access_token": generate_token(user), "token_type": "bearer"}
 
 
+@router.post("/admin/login")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    """aвторизауия пользователя по Auth2"""
+    try:
+        user = await Users.get(username=form_data.username)
+        if not user.superuser:
+            raise HTTPException(status_code=403, detail="Недостаточно прав")
+    except DoesNotExist as e:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    if not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=400, detail="Incorrect password")
+
+    return {"access_token": generate_token(user), "token_type": "bearer"}
+
 @router.get("/users/me")
 async def read_users_me(current_user: Users = Depends(get_current_active_user)):
     return UserFront_Pydantic.from_orm(current_user)
